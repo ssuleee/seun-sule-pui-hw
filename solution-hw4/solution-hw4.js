43db33d3cf750ba2cc03a9c3c4bd00b4c8937142
ssuleee <ssule@andrew.cmu.edu>
2024-09-25 16:15:53 -0400

    you see the array in the text

--- solution-hw4/solution-hw4.js
+++ solution-hw4/solution-hw4.js
@@ -1,11 +1,20 @@
+class Roll {
+  constructor(rollType, rollGlazing, packSize, basePrice) {
+    this.type = rollType;
+    this.glazing = rollGlazing;
+    this.size = packSize;
+    this.basePrice = basePrice;
+  }
+}
+
 const queryString = window.location.search;
 const params = new URLSearchParams(queryString);
 const rollType = params.get("roll");
 
-let cart = [];
+let cartBotton = [];
 let originalPrice = 0;
 
-// Function to populate the page with the roll information
+// Function to refresh product details
 function refreshPd() {
   const rollData = rolls[rollType];
 
@@ -27,6 +36,7 @@ function refreshPd() {
   }
 }
 
+// Function to show dropdown options for glazing and packing
 function showDropdown() {
   const glazeDropdown = document.getElementById("glazing");
   const packDropdown = document.getElementById("pack-size");
@@ -36,34 +46,35 @@ function showDropdown() {
 
   glazeChoice.forEach((glaze) => {
     const option = document.createElement("option");
-    option.value = glaze.price;
+    option.value = glaze.price; // You can keep this for reference
     option.textContent = glaze.description;
+    option.dataset.price = glaze.price; // Add price as a data attribute
     glazeDropdown.appendChild(option);
   });
 
   packingSize.forEach((pack) => {
     const option = document.createElement("option");
-    option.value = pack.increase;
+    option.value = pack.increase; // You can keep this for reference
     option.textContent = pack.size;
+    option.dataset.increase = pack.increase; // Add increase as a data attribute
     packDropdown.appendChild(option);
   });
-
-  console.log("Glaze options:", glazeDropdown.innerHTML);
-  console.log("Pack options:", packDropdown.innerHTML);
 
   glazeDropdown.addEventListener("change", updatePrice);
   packDropdown.addEventListener("change", updatePrice);
 }
 
+// Function to update price based on selected options
 function updatePrice() {
   const glazeDropdown = document.getElementById("glazing");
   const packDropdown = document.getElementById("pack-size");
 
-  const selectedGlazePrice = parseFloat(glazeDropdown.value);
-  const selectedPackIncrease = parseInt(packDropdown.value);
-
-  console.log("Selected glaze price:", selectedGlazePrice);
-  console.log("Selected pack increase:", selectedPackIncrease);
+  const selectedGlazePrice = Number(
+    glazeDropdown.options[glazeDropdown.selectedIndex].dataset.price
+  );
+  const selectedPackIncrease = Number(
+    packDropdown.options[packDropdown.selectedIndex].dataset.increase
+  );
 
   const finalPrice =
     (originalPrice + selectedGlazePrice) * selectedPackIncrease;
@@ -71,5 +82,46 @@ function updatePrice() {
   const priceElement = document.querySelector(".add-box-together p");
   priceElement.textContent = `$${finalPrice.toFixed(2)}`;
 }
+
+// Function to add selected roll to cart
+function addToCart() {
+  const glazeDropdown = document.getElementById("glazing");
+  const packDropdown = document.getElementById("pack-size");
+
+  const selectedGlazeDescription =
+    glazeDropdown.options[glazeDropdown.selectedIndex].text;
+  const selectedPackSize =
+    packDropdown.options[packDropdown.selectedIndex].text;
+
+  // Get the price directly from the data attributes
+  const selectedGlazePrice = Number(
+    glazeDropdown.options[glazeDropdown.selectedIndex].dataset.price
+  );
+  const selectedPackIncrease = Number(
+    packDropdown.options[packDropdown.selectedIndex].dataset.increase
+  );
+
+  // Calculate the final price
+  const finalPrice =
+    (originalPrice + selectedGlazePrice) * selectedPackIncrease;
+
+  // Create a new instance of Roll
+  const newRoll = new Roll(
+    rollType,
+    selectedGlazeDescription,
+    selectedPackSize,
+    finalPrice
+  );
+
+  // Add to cart
+  cartBotton.push(newRoll);
+
+  // Print the entire cart array to the console
+  console.log(cartBotton);
+}
+
+// Add event listener to the "Add to Cart" button with the class
+const addToCartButton = document.querySelector(".add-to-cart");
+addToCartButton.addEventListener("click", addToCart);
 
 document.addEventListener("DOMContentLoaded", refreshPd);
