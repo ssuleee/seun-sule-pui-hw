98d7a54203c82c04876a65696a57b98b79ea4e2a
ssuleee <ssule@andrew.cmu.edu>
2024-10-04 21:21:11 -0400

    Update solution-hw5.js
    removed notes from lab 5 that helped

--- solution-hw5/solution-hw5.js
+++ solution-hw5/solution-hw5.js
@@ -111,47 +111,26 @@ function addToCart() {
   console.log(cartBotton);
 }
 
-// const addToCartButton = document.querySelector(".add-to-cart");
-// addToCartButton.addEventListener("click", addToCart);
-
 document.addEventListener("DOMContentLoaded", refreshPd);
-
-// hw5
-// class Roll {
-//   constructor(rollType, rollGlazing, packSize, basePrice) {
-//     this.type = rollType;
-//     this.glazing = rollGlazing;
-//     this.size = packSize;
-//     this.basePrice = basePrice;
-
-//     // this.element = null;
-//   }
-// }
 
 const rollSet = new Set();
 
 let totalCheckOutPrice = 0;
 
 function addNewRoll(rollType, rollGlazing, packSize, basePrice) {
-  // Create a new notecard object. The Notecard constructor takes three
-  // arguments: the image URL, title text,  and body text.
   const bunbun = new Roll(rollType, rollGlazing, packSize, basePrice);
-  totalCheckOutPrice += bunbun.basePrice; // Use the base price directly
+  totalCheckOutPrice += bunbun.basePrice;
   console.log(totalCheckOutPrice);
-  // Add the notecard object to our notecard Set, which keeps track of all
-  // the notecards in our application.
+
   rollSet.add(bunbun);
 
   return bunbun;
 }
 
 function createElement(bunbun) {
-  // make a clone of the notecard template
   const template = document.querySelector("#cart-template");
   const clone = template.content.cloneNode(true);
 
-  // connect this clone to our notecard.element
-  // from this point we only need to refer to notecard.element
   bunbun.element = clone.querySelector(".shop-cart-everything");
 
   const btnDelete = bunbun.element.querySelector(".remove-text-under-picture");
@@ -160,17 +139,13 @@ function createElement(bunbun) {
     deleteRoll(bunbun);
   });
 
-  // add the notecard clone to the DOM
-  // find the notecard parent (#notecard-list) and add our notecard as its child
   const rollListElement = document.querySelector(".cart-page-all-together");
   rollListElement.prepend(bunbun.element);
 
-  // populate the notecard clone with the actual notecard content
   updateElement(bunbun);
 }
 
 function updateElement(bunbun) {
-  // get the HTML elements that need updating
   const rollImageElement = bunbun.element.querySelector(
     ".orginal-product-gallery-img"
   );
@@ -200,17 +175,13 @@ for (const bunbun of rollSet) {
 }
 
 function deleteRoll(bunbun) {
-  // Remove the notecard DOM object from the UI
   bunbun.element.remove();
 
-  // Remove the actual Roll object from our rollSet
-  rollSet.delete(bunbun); // Use delete instead of pop for Sets
+  rollSet.delete(bunbun);
 
-  // Decrease total price
   totalCheckOutPrice -= bunbun.basePrice;
   console.log(totalCheckOutPrice);
 
-  // Update the total price display
   const totalPriceElement = document.querySelector(".final-cart-price");
   totalPriceElement.innerText = "$" + totalCheckOutPrice.toFixed(2);
 }
